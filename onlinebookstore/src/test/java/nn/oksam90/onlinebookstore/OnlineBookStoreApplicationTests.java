90a82d7c74920817d1d9f3e715a4270e94b6e8da
HP <HP@DESKTOP-1EEP6R9>
2020-03-04 17:15:20 +0100

    Initial commit. created spring boot project the application properties

---
+++ onlinebookstore/src/test/java/nn/oksam90/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package nn.oksam90.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
